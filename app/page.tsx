39f4ea50a4a6b82b399148554d83044eeb066b98
Zack Borges <77366250+zackartborges@users.noreply.github.com>
2022-12-06 11:50:21 -0500

    initial commit: adds tailwind/ updates to new file structure

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+
+export default function Page() {
+  return (
+    <div className='bg-slate-800 h-screen w-screen flex justify-center items-center'>
+      <h1 className='text-lg text-slate-100 font-bold'>
+        Hello, world
+      </h1>
+    </div>
+  );
+}
